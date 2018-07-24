442fc1e5fd6009c218eb20464e48bb278efe6661
1498377951@qq.com <1498377951@qq.com>
2018-07-24 14:31:28 +0800

    实体类生成操作

---
+++ com-baiye-spring-cloud-databases/src/main/java/com/baiye/com/bai/ye/spring/cloud/databases/ComBaiyeSpringCloudDatabasesApplication.java
@@ -0,0 +1,12 @@
+package com.baiye.com.bai.ye.spring.cloud.databases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComBaiyeSpringCloudDatabasesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComBaiyeSpringCloudDatabasesApplication.class, args);
+    }
+}
---
+++ com-baiye-spring-cloud-databases/src/test/java/com/baiye/com/bai/ye/spring/cloud/databases/ComBaiyeSpringCloudDatabasesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.baiye.com.bai.ye.spring.cloud.databases;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComBaiyeSpringCloudDatabasesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
