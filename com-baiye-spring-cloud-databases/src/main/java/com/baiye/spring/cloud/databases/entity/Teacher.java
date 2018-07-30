57b86cb831f51e8ad5f2fb7a5be6a21575f3a253
1498377951@qq.com <1498377951@qq.com>
2018-07-30 20:55:10 +0800

    更改bug

---
+++ com-baiye-spring-cloud-databases/src/main/java/com/baiye/spring/cloud/databases/entity/Teacher.java
@@ -0,0 +1,33 @@
+package com.baiye.spring.cloud.databases.entity;
+
+public class Teacher {
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+}
